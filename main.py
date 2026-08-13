b3974cf3a2e55f73b7098ff2aa7ce2b04e98efde
joannyambura24-gif <joan.nyambura24@students.dkut.ac.ke>
2026-08-13 14:54:42 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+from database import create_db
+from routers.products import router as product_router
+from routers.suppliers import router as supplier_router
+
+app = FastAPI(
+    title="TechVault Product API",
+    version="1.0.0"
+)
+
+create_db()
+
+app.include_router(product_router)
+app.include_router(supplier_router)
+
+@app.get("/")
+def home():
+    return {"message": "Welcome to TechVault Product API"}
